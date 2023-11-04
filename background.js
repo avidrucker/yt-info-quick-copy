99e570552aba352ff68ea1a64479ff2f2dad5e5a
avidrucker <avidrucker@gmail.com>
2023-11-04 14:39:02 -0400

    add promise to service worker

--- background.js
+++ background.js
@@ -1,11 +1,44 @@
-chrome.commands.onCommand.addListener((command) => {
+chrome.commands.onCommand.addListener(async (command) => {
   if (command === "copy-info") {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.scripting.executeScript({
-        target: {tabId: tabs[0].id},
-        files: ['content.js']
+    try {
+      // Wrap chrome.tabs.query in a promise
+      const tabs = await new Promise((resolve, reject) => {
+        chrome.tabs.query({active: true, currentWindow: true}, (result) => {
+          if (chrome.runtime.lastError) {
+            reject(new Error(chrome.runtime.lastError.message));
+          } else {
+            resolve(result);
+          }
+        });
       });
-    });
+
+      if (tabs.length === 0 || !tabs[0]) {
+        console.error('No active tab found.');
+        return;
+      }
+
+      const tabId = tabs[0].id;
+      if (!tabId) {
+        console.error('Active tab has no ID:', tabs[0]);
+        return;
+      }
+
+      // Wrap chrome.scripting.executeScript in a promise
+      await new Promise((resolve, reject) => {
+        chrome.scripting.executeScript({
+          target: {tabId: tabId},
+          files: ['content.js']
+        }, () => {
+          if (chrome.runtime.lastError) {
+            reject(new Error(chrome.runtime.lastError.message));
+          } else {
+            resolve();
+          }
+        });
+      });
+    } catch (error) {
+      console.error('Error executing script:', error);
+    }
   }
 });
 
